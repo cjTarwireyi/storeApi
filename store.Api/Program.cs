3ba7d5d17bd729f4760aa0f002240ddfcc3ab082
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-08-26 20:29:57 +0200

    swagger congfig edit

--- store.Api/Program.cs
+++ store.Api/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.OpenApi.Models;
 using store.DataLayer.Services;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("V1", new OpenApiInfo
+    {
+        Title = "Store API",
+        Description = "An ASP.Net Api that enables you to work with retail stoere data",
+        Contact = new OpenApiContact
+        {
+            Name = "Cornelious Tarwireyi",
+            Email = "cornelioustarwireyi@gmail.com",
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/license/mit/")
+        },
+        Version = "v1"
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().FullName}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+}).AddSwaggerGen();
 
 builder.Services.AddTransient<IProductService, ProductService>();
 
