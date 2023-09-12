c966783236fb06333a95e4d5a134ffbe667eb66c
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-09-12 21:50:00 +0200

    added sale table

--- store.DataLayer/Data/StoreDBContext.cs
+++ store.DataLayer/Data/StoreDBContext.cs
@@ -12,11 +12,21 @@ namespace store.Api.Data
 
         }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Product>(entity =>
             {
                 entity.HasIndex(e => e.ProductCode).IsUnique();
+            });
+            builder.Entity<Sale>(entity =>
+            {
+                entity.HasIndex(e => e.ProductId)
+                .IsUnique();
+
+                //entity.HasOne(e => e.ProductId)
+                //.WithOne()
+                //.HasForeignKey(e => e.ProductId);
             });
             base.OnModelCreating(builder);
         }
---
+++ store.DataLayer/Model/Sale.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store.DataLayer.Model
+{
+    public class Sale
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        [MaxLength(100)]
+        public string? LastModifiedUser { get; set; }
+    }
+}
