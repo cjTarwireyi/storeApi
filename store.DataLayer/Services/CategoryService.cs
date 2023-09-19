3d11b6164a8b67cde4c78ad7a95d0f535e7e5c15
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-09-19 21:56:06 +0200

    category interface implementation

---
+++ store.DataLayer/Services/CategoryService.cs
@@ -0,0 +1,37 @@
+﻿using store.DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store.DataLayer.Services
+{
+    internal class CategoryService : ICategory
+    {
+        public Task<bool> AddCategory(Category Category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCategory(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Category>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category?> GetCategory(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCategory(Category Category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
