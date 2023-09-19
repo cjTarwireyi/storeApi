1f9cec1991a938dd4b32b4001f263eb7a64ccc5c
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-09-19 21:54:41 +0200

    added category interface

---
+++ store.DataLayer/Services/ICategory.cs
@@ -0,0 +1,18 @@
+﻿using store.DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store.DataLayer.Services
+{
+    internal interface ICategory
+    {
+        Task<List<Category>> GetCategories();
+        Task<Category?> GetCategory(string id);
+        Task<bool> AddCategory(Category Category);
+        Task<bool> UpdateCategory(Category Category);
+        Task<bool> DeleteCategory(string id);
+    }
+}
