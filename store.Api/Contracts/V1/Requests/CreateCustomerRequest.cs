cc6a29843a3b4df8bff647ec6724fac9869965f6
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-09-26 21:49:24 +0200

    added customer request

---
+++ store.Api/Contracts/V1/Requests/CreateCustomerRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace store.Api.Contracts.V1.Requests
+{
+    public class CreateCustomerRequest
+    {
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+     
+        public string Phone { get; set; }
+       
+        public string? LastModifiedUser { get; set; }
+    }
+}
