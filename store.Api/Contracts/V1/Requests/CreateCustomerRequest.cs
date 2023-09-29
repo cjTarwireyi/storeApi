9b4c7cd85aed4087ea16df45f5c7f3b0038c49aa
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-09-29 17:56:12 +0200

    made fields nullable on some of the requests models

--- store.Api/Contracts/V1/Requests/CreateCustomerRequest.cs
+++ store.Api/Contracts/V1/Requests/CreateCustomerRequest.cs
@@ -6,12 +6,12 @@ namespace store.Api.Contracts.V1.Requests
     {
 
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
      
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
        
         public string? LastModifiedUser { get; set; }
     }
