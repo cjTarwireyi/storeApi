25b46e9096db90158d664a1127d80a7bc5cc49f6
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-09-30 20:40:01 +0200

    additional nullable fields to request models

--- store.Api/Contracts/V1/Requests/CreateOrderRequest.cs
+++ store.Api/Contracts/V1/Requests/CreateOrderRequest.cs
@@ -5,7 +5,7 @@ namespace store.Api.Contracts.V1.Requests
     public class CreateOrderRequest
     {     
         public Guid CustomerId { get; set; }
-        public string OrderNumber { get; set; }
+        public string? OrderNumber { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
         public DateTime OrderDate { get; set; }
--- store.Api/Contracts/V1/Requests/UpdateCustomerRequest.cs
+++ store.Api/Contracts/V1/Requests/UpdateCustomerRequest.cs
@@ -3,11 +3,11 @@
     public class UpdateCustomerRequest
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Address { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
 
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
     }
 }
