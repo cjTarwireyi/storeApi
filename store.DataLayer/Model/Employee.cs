659895c1368f1e0fb81c8041b5a345b2ce5221f2
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-10-01 21:20:22 +0200

    added employee DB entity

---
+++ store.DataLayer/Model/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace store.DataLayer.Model
+{
+    internal class Employee
+    {
+        public Guid Id { get; set; }
+        [MaxLength(13)]
+        [MinLength(13)]
+        public string? IdNr { get; set; }
+        public string? PassportNr { get; set; }
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        [MaxLength(500)]
+        public string Email { get; set; }
+        [MaxLength(100)]
+        public string Address { get; set; }
+        [MaxLength(100)]
+        public string Phone { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+        [MaxLength(100)]
+        public string? LastModifiedUser { get; set; }
+    }
+}
