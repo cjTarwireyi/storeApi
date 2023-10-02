ac019cf4151f1447833fe75bc57263267336ebd4
cornelious Tarwireyi <cornelioustarwireyi@gmail.com>
2023-10-02 20:39:55 +0200

    Created employee interface

---
+++ store.DataLayer/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using store.DataLayer.Model;
+
+namespace store.DataLayer.Services
+{
+    internal interface IEmployeeService
+    {
+        Task<List<Employee>> GetEmployees();
+        Task<Employee?> GetEmployee(string id);
+        Task<bool> AddEmployee(Employee customer);
+        Task<bool> UpdateEmployee(Employee customer);
+        Task<bool> DeleteEmployee(string id);
+    }
+}
